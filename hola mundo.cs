b9157bf32348de9d9ce4154b7183783a21fd086f
Jhonny Fuertes <jfuertes@unicauca.edu.co>
2021-11-21 18:48:39 -0500

    primera fase del proyecto

---
+++ hola mundo.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hola-mundo
+{
+    internal class holaMundo
+    {
+        static void Main(string[] args)
+        {
+          Console.WriteLine("hola munddo"):
+        }
+      }
+    }
+
+//prueba para probar lo de las ramas 
